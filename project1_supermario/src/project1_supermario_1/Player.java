5f813f75fb2051e8066aeca92de278a1848cf34b
heejin <tkdwls1229@naver.com>
2022-04-12 15:02:01 +0900

    플레이어 위치 변수설정

--- project1_supermario/src/project1_supermario_1/Player.java
+++ project1_supermario/src/project1_supermario_1/Player.java
@@ -1,5 +1,22 @@
 package project1_supermario_1;
 
 public class Player {
-
+	// 위치
+	private int playerX;
+	private int playerY;
+	
+	public int getPlayerX() {
+		return playerX;
+	}
+	public void setPlayerX(int playerX) {
+		this.playerX = playerX;
+	}
+	public int getPlayerY() {
+		return playerY;
+	}
+	public void setPlayerY(int playerY) {
+		this.playerY = playerY;
+	}
+	
+	
 }
