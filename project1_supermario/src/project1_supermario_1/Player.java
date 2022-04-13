d866e8443a33cbbf62b5dc142f2172faa77ec455
heejin <tkdwls1229@naver.com>
2022-04-13 10:54:40 +0900

    코드수정

--- project1_supermario/src/project1_supermario_1/Player.java
+++ project1_supermario/src/project1_supermario_1/Player.java
@@ -1,5 +1,7 @@
 package project1_supermario_1;
 
+import java.awt.Image;
+
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
@@ -8,7 +10,10 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Player extends JLabel implements Moveable {
+public class Player extends SuperMarioFrame implements Moveable {
+	
+	SuperMarioFrame marioFrame;
+	
 	// 위치
 	private int playerX;
 	private int playerY;
@@ -30,8 +35,15 @@ public class Player extends JLabel implements Moveable {
 	private boolean rightwallCrash;
 
 	// 이미지
-	private ImageIcon playerR;
-	private ImageIcon playerL;
+	private Image player;
+	private Image playerR;
+	private Image playerR2;
+	private Image playerL;
+	private Image playerL2;
+	private Image jumpR;
+	private Image jumpL;
+	
+	
 
 	public Player() {
 		initObject();
@@ -39,8 +51,13 @@ public class Player extends JLabel implements Moveable {
 	}
 
 	private void initObject() {
-		playerR = new ImageIcon("images/mario_right.png");
-		playerL = new ImageIcon("images/mario_left.png");
+		playerR = new ImageIcon("images/mario_right.png").getImage();
+		playerR2 = new ImageIcon("images/mario1_right.png").getImage();
+		playerL = new ImageIcon("images/mario_left.png").getImage();
+		playerL2 = new ImageIcon("images/mario1_left.png").getImage();
+		jumpL = new ImageIcon("images/jump_left.png").getImage();
+		jumpR = new ImageIcon("images/jump_right.png").getImage();
+		player = playerR;
 	}
 
 	private void initSetting() {
@@ -54,9 +71,6 @@ public class Player extends JLabel implements Moveable {
 		leftwallCrash = false;
 		rightwallCrash = false;
 		// 크기, 위치 지정
-		setIcon(playerR);
-		setSize(100, 100);
-		setLocation(playerX, playerX);
 	}
 
 	@Override
@@ -67,9 +81,12 @@ public class Player extends JLabel implements Moveable {
 			@Override
 			public void run() {
 				while (left) {
-					setIcon(playerL);
+					player = playerL;
 					playerX -= playerX;
-					setLocation(playerX, playerY);
+					player = playerL2;
+					playerX -= playerX;
+//					setLocation(playerX, playerY)
+					panel.repaint();
 					threadSleep(10);
 				}
 				left = false;
@@ -86,12 +103,13 @@ public class Player extends JLabel implements Moveable {
 			@Override
 			public void run() {
 				while (right) {
-					setIcon(playerR);
+//					setIcon(playerR);
+					player = playerR;
 					playerX -= playerX;
-					setLocation(playerX, playerY);
-					setIcon(playerL);
+					player = playerR2;
 					playerX -= playerX;
-					setLocation(playerX, playerY);
+					panel.repaint();
+//					setLocation(playerX, playerY);
 					threadSleep(10);
 				}
 				right = false;
@@ -108,8 +126,11 @@ public class Player extends JLabel implements Moveable {
 			@Override
 			public void run() {
 				for (int i = 0; i < (130 / JUMPSPEED); i++) {
+					
+					player = jumpL;
 					playerY -= JUMPSPEED;
-					setLocation(playerX, playerY);
+//					setLocation(playerX, playerY);
+					panel.repaint();
 					threadSleep(5);
 				}
 				up = false;
@@ -128,7 +149,8 @@ public class Player extends JLabel implements Moveable {
 			public void run() {
 				for (int i = 0; i < (130 / JUMPSPEED); i++) {
 					playerY += JUMPSPEED;
-					setLocation(playerX, playerY);
+//					setLocation(playerX, playerY);
+					panel.repaint();
 					threadSleep(3);
 				}
 				down = false;
